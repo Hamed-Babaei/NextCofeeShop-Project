f934198a167ad7e961d877fde53a18a1455462a2
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-01 12:04:16 +0330

    all new bugs are fixed

--- next-cofee/pages/index.js
+++ next-cofee/pages/index.js
@@ -2,7 +2,11 @@ import Slider from "@/components/templates/Index/Slider";
 import React from "react";
 
 function Index() {
-  return <>{/* <Slider /> */}</>;
+  return (
+    <>
+      <Slider />
+    </>
+  );
 }
 
 export default Index;
