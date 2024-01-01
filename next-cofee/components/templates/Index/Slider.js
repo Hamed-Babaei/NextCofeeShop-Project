a39ba9bf0d753bfb36ffa1dddf3a4aa83b31aece
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-01 12:33:28 +0330

    Update Swiper library and add autoplay functionality to the slider.

--- next-cofee/components/templates/Index/Slider.js
+++ next-cofee/components/templates/Index/Slider.js
@@ -4,15 +4,52 @@ import "@/styles/Slider.module.css";
 import "swiper/css";
 import "swiper/css/navigation";
 
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
+
+import styles from "@/styles/Slider.module.css";
 
 function Slider() {
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        className={styles.swiper}
+        loop={true}
+        delay={1}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide
+          style={{ backgroundImage: 'url("/images/carousel-1.jpg")' }}
+          className={styles.swiper_slide}
+        >
+          <div
+            className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+          >
+            <h2 className="text-primary font-weight-medium m-0">
+              We Have Been Serving
+            </h2>
+            <h1 className="display-1 text-white m-0">COFFEE</h1>
+            <h2 className="text-white m-0">* SINCE 1950 *</h2>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide
+          style={{ backgroundImage: 'url("/images/carousel-2.jpg")' }}
+          className={styles.swiper_slide}
+        >
+          <div
+            className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}
+          >
+            <h2 className="text-primary font-weight-medium m-0">
+              We Have Been Serving
+            </h2>
+            <h1 className="display-1 text-white m-0">COFFEE</h1>
+            <h2 className="text-white m-0">* SINCE 1950 *</h2>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
