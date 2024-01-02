c0dbedc6d9a4b3c483f25be6ee020e7fe88f3aea
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-02 23:32:06 +0330

    Add Comments component and fetch comments data

--- next-cofee/components/templates/Testimonial/Comments.js
+++ next-cofee/components/templates/Testimonial/Comments.js
@@ -1,7 +1,29 @@
+import Testimonial from "@/components/modules/Testimonial/Testimonial";
 import React from "react";
 
-function Comments() {
-  return <div>Comments</div>;
+function Comments({ data }) {
+  return (
+    <>
+      <div className="container-fluid py-5">
+        <div className="container">
+          <div className="section-title">
+            <h4
+              className="text-primary text-uppercase"
+              style={{ letter_spacing: "5px" }}
+            >
+              Testimonial
+            </h4>
+            <h1 className="display-4">Our Clients Say</h1>
+          </div>
+          <div className="owl-carousel testimonial-carousel">
+            {data.slice(0, 5).map((item) => (
+              <Testimonial key={item.id} {...item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Comments;
--- next-cofee/pages/testimonial/index.js
+++ next-cofee/pages/testimonial/index.js
@@ -1,12 +1,24 @@
 import PageHeader from "@/components/modules/PageHeader/PageHeader";
+import Comments from "@/components/templates/Testimonial/Comments";
 import React from "react";
 
 function Testimonial({ comment }) {
   return (
     <>
       <PageHeader route={"Testimonial"} />
+      <Comments data={comment} />
     </>
   );
 }
+export async function getStaticProps() {
+  const commentsResponse = await fetch(`http://localhost:4000/comments`);
+  const commentsData = await commentsResponse.json();
 
+  return {
+    props: {
+      comment: commentsData,
+    },
+    revalidate: 60 * 60 * 12,
+  };
+}
 export default Testimonial;
