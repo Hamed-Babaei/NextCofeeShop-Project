b185e6dfa291d3bec5f34a7d9579a3abf4dedbf0
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-01-04 14:13:47 +0330

    Card Component is Refactored

--- next-cofee/components/modules/Card/Card.js
+++ next-cofee/components/modules/Card/Card.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-function Card() {
-  return <div>Card</div>;
+function Card({ id, title, img, desc, price }) {
+  return (
+    <>
+      <div key={id} className="row align-items-center mb-5">
+        <div className="col-4 col-sm-3">
+          <img
+            className="w-100 rounded-circle mb-3 mb-sm-0"
+            style={{ height: "112px" }}
+            src={img}
+            alt=""
+          />
+          <h5 className="menu-price">${price}</h5>
+        </div>
+        <div className="col-8 col-sm-9">
+          <h4>{title}</h4>
+          <p className="m-0">{desc}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
--- next-cofee/components/templates/Menu/Pricing.js
+++ next-cofee/components/templates/Menu/Pricing.js
@@ -1,7 +1,7 @@
+import Card from "@/components/modules/Card/Card";
 import React from "react";
 
 function Pricing({ data }) {
-  console.log(data);
   return (
     <>
       <div className="container-fluid pt-5">
@@ -23,20 +23,7 @@ function Pricing({ data }) {
                 .filter((item) => item.type === "hot")
                 .slice(0, 3)
                 .map((item) => (
-                  <div key={item.id} className="row align-items-center mb-5">
-                    <div className="col-4 col-sm-3">
-                      <img
-                        className="w-100 rounded-circle mb-3 mb-sm-0"
-                        src={item.img}
-                        alt=""
-                      />
-                      <h5 className="menu-price">${item.price}</h5>
-                    </div>
-                    <div className="col-8 col-sm-9">
-                      <h4>{item.title}</h4>
-                      <p className="m-0">{item.desc}</p>
-                    </div>
-                  </div>
+                  <Card key={item.id} {...item} />
                 ))}
             </div>
             <div className="col-lg-6">
@@ -46,21 +33,7 @@ function Pricing({ data }) {
                 .filter((item) => item.type === "cold")
                 .slice(0, 3)
                 .map((item) => (
-                  <div key={item.id} className="row align-items-center mb-5">
-                    <div className="col-4 col-sm-3">
-                      <img
-                        className="w-100 rounded-circle mb-3 mb-sm-0"
-                        style={{ height: "112px" }}
-                        src={item.img}
-                        alt=""
-                      />
-                      <h5 className="menu-price">${item.price}</h5>
-                    </div>
-                    <div className="col-8 col-sm-9">
-                      <h4>{item.title}</h4>
-                      <p className="m-0">{item.desc}</p>
-                    </div>
-                  </div>
+                  <Card key={item.id} {...item} />
                 ))}
             </div>
           </div>
